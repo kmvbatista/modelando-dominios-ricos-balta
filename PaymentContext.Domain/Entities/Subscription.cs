3c1922023a5009803cd6cbae14bcd34220982c9b
Kennedy <kennedymnovas@gmail.com>
2019-10-17 21:31:22 -0300

    Subscription

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -5,6 +5,25 @@ namespace  PaymentContext.Domaint.Entities
 {
   public class Subscription
   {
-    
+    private IList<Payment> _payments;
+    public Subscription(DateTime? expireDate)
+    {
+      CreationDate = DateTime.Now;
+      LastUpdateDate = DateTime.Now;
+      Active = true;
+      ExpireDate = expireDate;
+      _payments = new List<Payment>();
+    }
+
+    public DateTime CreationDate { get;  private set; }
+      public DateTime LastUpdateDate { get;  private set; }
+      public DateTime? ExpireDate { get;  private set; }
+      public bool Active { get; set; }
+      public IReadOnlyCollection<Payment> Payments { get; set; }
+
+      public void AddPayments() 
+      {
+
+      }
     }
 }
